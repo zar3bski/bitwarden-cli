395f845b7af37ed805a0193ce977ad99eb5acb20
zar3bski <zarebskidavid@gmail.com>
2025-06-23 17:30:14 +0200

    feat: livenessProbe

---
+++ scripts/livenessProbe.sh
@@ -0,0 +1,10 @@
+#!/bin/ash
+
+code=$(wget -S -q http://127.0.0.1:8087/sync?force=true --post-data='' -O /dev/null 2>&1 | grep "HTTP/" | awk '{print $2}')
+
+if [[ $code != "200" ]]; then
+   echo "[WARN] livenessProbe got HTTP status code != 200"
+   exit 1
+else
+   exit 0
+fi
