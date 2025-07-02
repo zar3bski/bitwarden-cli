4607d9a9c9ec835c138c4ce6a546d0168dae21a9
zar3bski <zarebskidavid@gmail.com>
2025-07-02 10:59:44 +0200

    fix: liveness probe for 5**

--- scripts/livenessProbe.sh
+++ scripts/livenessProbe.sh
@@ -1,6 +1,6 @@
 #!/bin/ash
 
-code=$(wget -S -q http://127.0.0.1:8087/sync?force=true --post-data='' -O /dev/null 2>&1 | grep "HTTP/" | awk '{print $2}')
+code=$(wget -S -q http://127.0.0.1:8087/sync?force=true --post-data='' -O /dev/null 2>&1 | grep -m 1 "HTTP/" | awk '{print $2}')
 
 if [[ $code != "200" ]]; then
    echo "[WARN] livenessProbe got HTTP status code != 200"
